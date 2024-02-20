3ca94e70897e8c4bb9a388002385ebdb4f00b8bb
Defalt2-O <rithinpaul10@gmail.com>
2024-02-20 23:25:36 +0530

    Dummy ListView for list of expenses created.

---
+++ lib/expenses_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker_app/models/expense.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({super.key, required this.expenses});
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(context) {
+    return ListView.builder(
+      itemCount: expenses
+          .length, //Item count runs the function below, as many times as its value.
+      //index starts from 0 and runs the function until it is equal to itemCount
+      itemBuilder: (ctx, index) => Text(expenses[index].title),
+    );
+    //ListView returns a widget that displays a scrolling list of child widgets in the form of a column.
+    //Howeever, we also use builder with it as ListView, without builder, creates all scrollable items when it is called.
+    //Whereas builder allows for creation of those items, only when they are present on the screen.
+  }
+}
