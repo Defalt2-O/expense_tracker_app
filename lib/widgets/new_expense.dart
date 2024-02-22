2ae63db256f8f0a803b4005e53e9ed7c6033c566
Defalt2-O <rithinpaul10@gmail.com>
2024-02-22 12:55:28 +0530

    Input Title Saved to variable.

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,19 +10,36 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  var _enteredValue = '';
+
+  void _saveTitleInput(String inputValue) {
+    _enteredValue = inputValue;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            onChanged:
+                _saveTitleInput, //onChanged is executed each time the TextField widget value changes.
             //Used to accept inputs from the user
             maxLength: 50,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               //In order for a label to be provided, we must access the decoration argument first
               label: Text('Title'),
             ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print(_enteredValue);
+                  },
+                  child: const Text('Save Expense'))
+            ],
           ),
         ],
       ),
