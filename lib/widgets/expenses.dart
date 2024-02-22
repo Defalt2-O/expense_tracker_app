013117b27504fcde52d8d393b65473e07458d4a1
Defalt2-O <rithinpaul10@gmail.com>
2024-02-22 12:36:31 +0530

    Modal Overlay's Title Input field added.

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:expense_tracker_app/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/models/expense.dart';
+import 'package:expense_tracker_app/widgets/new_expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -29,8 +30,10 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
-      context: context,
-      builder: (ctx) => const Text('Modal Overlay'),
+      context:
+          context, //context tells Flutter where the content should be displayed in the app.
+      builder: (ctx) =>
+          const NewExpense(), //similarly ctx is the context for the Modal Overlay.
     );
   }
 
---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() {
+    return _NewExpenseState();
+  }
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            //Used to accept inputs from the user
+            maxLength: 50,
+            decoration: InputDecoration(
+              //In order for a label to be provided, we must access the decoration argument first
+              label: Text('Title'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
