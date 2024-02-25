d92cfd37220a7abc7d2aa424dd5ef83e9428b417
Defalt2-O <rithinpaul10@gmail.com>
2024-02-25 18:19:20 +0530

    Theming added to MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:expense_tracker_app/widgets/expenses.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      // ignore: deprecated_member_use
+      theme: ThemeData().copyWith(
+          scaffoldBackgroundColor: const Color.fromARGB(255, 93, 194, 245)),
+      home: const Expenses(),
     ),
   );
 }
