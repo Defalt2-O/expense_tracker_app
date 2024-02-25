4d660408b924408c357edd7e932e31e4153d272b
Defalt2-O <rithinpaul10@gmail.com>
2024-02-25 19:58:07 +0530

    Appbar Theming ColorScheme added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/widgets/expenses.dart';
 
+var kcolorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 93, 194, 245),
+);
+
 void main() {
   runApp(
     MaterialApp(
-      // ignore: deprecated_member_use
       theme: ThemeData().copyWith(
-          scaffoldBackgroundColor: const Color.fromARGB(255, 93, 194, 245)),
+        colorScheme: kcolorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          foregroundColor: kcolorScheme.secondaryContainer,
+          backgroundColor: kcolorScheme.onSecondaryContainer,
+        ),
+      ),
       home: const Expenses(),
     ),
   );
