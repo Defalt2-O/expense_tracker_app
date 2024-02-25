e92e64084c974719e091f0c360672c31b16cf67a
Defalt2-O <rithinpaul10@gmail.com>
2024-02-25 21:39:45 +0530

    Theming modified to red. Card theme modified.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/widgets/expenses.dart';
 
 var kcolorScheme = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 93, 194, 245),
+  seedColor: const Color.fromARGB(255, 222, 46, 11),
 );
 
 void main() {
@@ -11,9 +11,25 @@ void main() {
       theme: ThemeData().copyWith(
         colorScheme: kcolorScheme,
         appBarTheme: const AppBarTheme().copyWith(
-          foregroundColor: kcolorScheme.secondaryContainer,
-          backgroundColor: kcolorScheme.onSecondaryContainer,
+          foregroundColor: kcolorScheme.primaryContainer,
+          backgroundColor: kcolorScheme.onPrimaryContainer,
         ),
+        cardTheme: const CardTheme().copyWith(
+          color: kcolorScheme.primaryContainer,
+          elevation: 5,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kcolorScheme.onSecondaryContainer,
+            foregroundColor: kcolorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: const TextStyle(fontWeight: FontWeight.normal),
+              bodyMedium: TextStyle(
+                  fontSize: 16, color: kcolorScheme.onPrimaryContainer),
+            ),
       ),
       home: const Expenses(),
     ),
